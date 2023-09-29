1d5871a7dcefef3c6a868c7135c1f3e5d3418bb8
Hessam Mahdiabadi <67460597+iamHEssam@users.noreply.github.com>
2023-09-29 23:38:19 +0330

    Initial Commit

---
+++ Library/LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryApp.swift
+//  Library
+//
+//  Created by Hessam Mahdiabadi on 9/29/23.
+//
+
+import SwiftUI
+
+@main
+struct LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
